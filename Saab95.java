8a71fb7481a6ac6da108152bc734332bd0ddbecb
MindokRuvaak <cole_daniel@rocketmail.com>
2024-01-18 15:23:52 +0100

    plan

--- Saab95.java
+++ Saab95.java
@@ -33,7 +33,8 @@ public class Saab95 extends Car {
     public Color getColor() {
         return color;
     }
-// hej
+
+    // hej
     public void setColor(Color clr) {
         color = clr;
     }
@@ -56,8 +57,9 @@ public class Saab95 extends Car {
 
     public double speedFactor() {
         double turbo = 1;
-        if (turboOn)
+        if (turboOn) {
             turbo = 1.3;
+        }
         return enginePower * 0.01 * turbo;
     }
 
--- plan.md
+++ plan.md
@@ -1,4 +1,16 @@
 # THE PLAN!
 
-## Superclass or Interface
+## Uppgift 1
 
+### Del 1
+- Skapa en abstrakt superklass (Car.java)
+- Volvo och Saab extends Car
+#### varför
+- mindre kodduplicering
+- gemensamma variabler
+- för och nackdel: type casting
+
+### Del 2
+ - getters bör vara public (I allmänhet)
+ - setter bör vara public (I allmänhet)
+ - hjälp
