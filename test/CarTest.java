8a7f8ddc57801f29404a8036a4628c2ff990564c
MindokRuvaak <cole_daniel@rocketmail.com>
2024-01-21 12:15:51 +0100

    forgot to commit on friday, test files created, nothing implemented yet

---
+++ test/CarTest.java
@@ -0,0 +1,74 @@
+package test;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    @Test
+    void testBrake() {
+
+    }
+
+    @Test
+    void testGas() {
+
+    }
+
+    @Test
+    void testGetColor() {
+
+    }
+
+    @Test
+    void testGetCurrentSpeed() {
+
+    }
+
+    @Test
+    void testGetDirection() {
+
+    }
+
+    @Test
+    void testGetEnginePower() {
+
+    }
+
+    @Test
+    void testGetNrDoors() {
+
+    }
+
+    @Test
+    void testMove() {
+
+    }
+
+    @Test
+    void testSetColor() {
+
+    }
+
+    @Test
+    void testSpeedFactor() {
+
+    }
+
+    @Test
+    void testStartEngine() {
+
+    }
+
+    @Test
+    void testStopEngine() {
+
+    }
+
+    @Test
+    void testTurnLeft() {
+
+    }
+
+    @Test
+    void testTurnRight() {
+
+    }
+}
---
+++ test/Saab95Test.java
@@ -0,0 +1,19 @@
+package test;
+import org.junit.jupiter.api.Test;
+
+public class Saab95Test {
+    @Test
+    void testSetTurboOff() {
+
+    }
+
+    @Test
+    void testSetTurboOn() {
+
+    }
+
+    @Test
+    void testSpeedFactor() {
+
+    }
+}
